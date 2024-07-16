dbac4297748abfb8c5e7985bfe4288a18dcf69b1
Delfin F <DelfinoDevShark@gmail.com>
2024-07-16 14:53:07 +0200

    html semantic

--- src/App.js
+++ src/App.js
@@ -93,104 +93,110 @@ function App() {
 	};
 
 	return (
-		<div className="app">
-			<div className="column">
-				<form onSubmit={handleSubmit}>
-					<header>
-						<h1>Mortgage Calculator</h1>
-						<Link onClick={handleClear}>Clear All</Link>
-					</header>
-					<div className="inputs">
-						<label htmlFor="amount">Mortgage Amount</label>
-						<Number
-							inputName={"amount"}
-							value={formData.amount}
-							onChange={handleChange}
-							min={0}
-							max={1000000}
-							textInput={"£"}
-							error={errors.amount}
-						/>
-						<div className="twoColumns">
-							<div className="twoColumns-column">
-								<label htmlFor="term">Mortgage Term</label>
+		<>
+			<header></header>
+			<main>
+				<div className="app">
+					<div className="column">
+						<form onSubmit={handleSubmit}>
+							<header>
+								<h1>Mortgage Calculator</h1>
+								<Link onClick={handleClear}>Clear All</Link>
+							</header>
+							<div className="inputs">
+								<label htmlFor="amount">Mortgage Amount</label>
 								<Number
-									inputName={"term"}
-									value={formData.term}
+									inputName={"amount"}
+									value={formData.amount}
 									onChange={handleChange}
 									min={0}
-									max={100}
-									textInput={"years"}
-									textImputRight={false}
-									error={errors.term}
+									max={1000000}
+									textInput={"£"}
+									error={errors.amount}
 								/>
-							</div>
-							<div className="twoColumns-column">
-								<label htmlFor="rate">Interest Rate</label>
-								<Number
-									inputName={"rate"}
-									value={formData.rate}
-									onChange={handleChange}
-									min={2}
-									max={100}
-									textInput={"%"}
-									textImputRight={false}
-									error={errors.rate}
-									isDecimalAllowed={true}
-								/>
-							</div>
-						</div>
+								<div className="twoColumns">
+									<div className="twoColumns-column">
+										<label htmlFor="term">Mortgage Term</label>
+										<Number
+											inputName={"term"}
+											value={formData.term}
+											onChange={handleChange}
+											min={0}
+											max={100}
+											textInput={"years"}
+											textImputRight={false}
+											error={errors.term}
+										/>
+									</div>
+									<div className="twoColumns-column">
+										<label htmlFor="rate">Interest Rate</label>
+										<Number
+											inputName={"rate"}
+											value={formData.rate}
+											onChange={handleChange}
+											min={2}
+											max={100}
+											textInput={"%"}
+											textImputRight={false}
+											error={errors.rate}
+											isDecimalAllowed={true}
+										/>
+									</div>
+								</div>
 
-						<section
-							className="mortgage-type"
-							aria-label="Mortgage Type Section"
-						>
-							<label htmlFor={`${formData.mortgageType}-repayment`}>
-								Mortgage Type
-							</label>
-							<Radios
-								inputName="mortgageType"
-								options={radioOptions}
-								selectedValue={formData.mortgageType}
-								onChange={handleChange}
-								error={errors.mortgageType}
-							/>
-						</section>
+								<section
+									className="mortgage-type"
+									aria-label="Mortgage Type Section"
+								>
+									<label htmlFor={`${formData.mortgageType}-repayment`}>
+										Mortgage Type
+									</label>
+									<Radios
+										inputName="mortgageType"
+										options={radioOptions}
+										selectedValue={formData.mortgageType}
+										onChange={handleChange}
+										error={errors.mortgageType}
+									/>
+								</section>
+							</div>
+							<button type="submit">
+								<img src="/images/icon-calculator.svg" alt="Calculator Icon" />
+								Calculate Repayments
+							</button>
+						</form>
 					</div>
-					<button type="submit">
-						<img src="/images/icon-calculator.svg" alt="Calculator Icon" />
-						Calculate Repayments
-					</button>
-				</form>
-			</div>
-			<div className="column right">
-				{results ? (
-					<>
-						<h1>Your results</h1>
-						<p>
-							Your results are shown below based on the information you
-							provided. To adjust the results, edit the form and click
-							"calculate repayments" again.
-						</p>
-						<div className="results">
-							<p>Your monthly repayments</p>
-							<h3>{results.monthlyRepayment}</h3>
-							<p>Total you'll repay over the term</p>
-							<h4>{results.totalRepayment}</h4>
-						</div>
-					</>
-				) : (
-					<>
-						<img src="/images/illustration-empty.svg" alt="results Icon" />
-						<h2>Results shown here</h2>
-						<p>
-							Complete the form and click "calculate repayments" to see what
-							your monthly repayments would be.
-						</p>
-					</>
-				)}
-			</div>
-		</div>
+					<div className="column right">
+						{results ? (
+							<>
+								<h1>Your results</h1>
+								<p>
+									Your results are shown below based on the information you
+									provided. To adjust the results, edit the form and click
+									"calculate repayments" again.
+								</p>
+								<div className="results">
+									<p>Your monthly repayments</p>
+									<h3>{results.monthlyRepayment}</h3>
+									<p>Total you'll repay over the term</p>
+									<h4>{results.totalRepayment}</h4>
+								</div>
+							</>
+						) : (
+							<>
+								<img src="/images/illustration-empty.svg" alt="results Icon" />
+								<h2>Results shown here</h2>
+								<p>
+									Complete the form and click "calculate repayments" to see what
+									your monthly repayments would be.
+								</p>
+							</>
+						)}
+					</div>
+				</div>
+			</main>
+			<footer></footer>
+		</>
 	);
 }
 
