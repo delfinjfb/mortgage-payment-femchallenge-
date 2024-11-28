2701dec4895411dc90e17a3f2c5ba8b115c954fe
Delfin F <DelfinoDevShark@gmail.com>
2024-11-28 03:06:00 +0100

    Matomo tracking code

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,17 +1,19 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
-import PrivacyPolicy from "../pages/PrivacyPolicy"; // Correct relative path
+import PrivacyPolicy from "../pages/PrivacyPolicy";
+import Matomo from "../utils/Matomo"; // Adjust the path accordingly
 
 function AppRouter() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/privacy-policy" element={<PrivacyPolicy />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Matomo />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRouter;
