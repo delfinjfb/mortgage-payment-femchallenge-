ed816341fad5c40fea4bb2975bde48e9343742ab
delfin <delfinjfb@gmail.com>
2025-01-22 23:31:37 +0100

    chore: Migrate ESLint configuration to flat config format with custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
-module.exports = {
-  extends: ['react-app', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
+// eslint.config.js (Flat Config)
+import js from '@eslint/js'; // ESLint's built-in JavaScript config
+
+export default [
+  // 1) Base config(s):
+  js.configs.recommended,
+
+  // 2) Additional or custom config object(s):
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules', 'dist'],
+    rules: {
+      // your overrides
+      'no-console': 'warn',
+    },
   },
-};
+];
